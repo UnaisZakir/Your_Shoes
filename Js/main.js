c8332e67c0daa804fb5803561d2a7f4b27b93b80
Unais <unaiszakir0321@gmail.com>
2024-05-14 00:18:16 +0500

    My First Repositories

---
+++ Js/main.js
@@ -0,0 +1,582 @@
+// Adidas AlphaBoost V1 Shoes Js Start
+function Adidasmen1() {
+    document.getElementById("adidasmen1").src = "Image/Adidas/Men/AdidasAlphaboost2.jpg"
+}
+function Adidasmen2() {
+    document.getElementById("adidasmen1").src = "Image/Adidas/Men/AdidasAlphaboost.jpg"
+}
+function aabv1() {
+    document.getElementById("aabv0").src = "Image/Adidas/Men/AdidasAlphaboost.jpg"
+}
+function aabv2() {
+    document.getElementById("aabv0").src = "Image/Adidas/Men/AdidasAlphaboost1.jpg"
+}
+function aabv3() {
+    document.getElementById("aabv0").src = "Image/Adidas/Men/AdidasAlphaboost2.jpg"
+}
+// Adidas AlphaBoost V1 Shoes Js End
+
+
+// ADIDAS GAZELLE SHOES Js Start
+function Adidasmen3() {
+    document.getElementById("adidasmen2").src = "Image/Adidas/Men/AdidasGazelle2.jpg"
+}
+function Adidasmen4() {
+    document.getElementById("adidasmen2").src = "Image/Adidas/Men/AdidasGazelle.jpg"
+}
+function aabv5() {
+    document.getElementById("aabv4").src = "Image/Adidas/Men/AdidasGazelle.jpg"
+}
+function aabv6() {
+    document.getElementById("aabv4").src = "Image/Adidas/Men/AdidasGazelle1.jpg"
+}
+function aabv7() {
+    document.getElementById("aabv4").src = "Image/Adidas/Men/AdidasGazelle2.jpg"
+}
+// ADIDAS GAZELLE SHOES Js End
+
+
+// ADIDAS LITE RACER 3.0 SHOES Js Start
+function Adidasmen5() {
+    document.getElementById("adidasmen3").src = "Image/Adidas/Men/AdidasLiteRacer2.jpg"
+}
+function Adidasmen6() {
+    document.getElementById("adidasmen3").src = "Image/Adidas/Men/AdidasLiteRacer.jpg"
+}
+function aabv9() {
+    document.getElementById("aabv8").src = "Image/Adidas/Men/AdidasLiteRacer.jpg"
+}
+function aabv10() {
+    document.getElementById("aabv8").src = "Image/Adidas/Men/AdidasLiteRacer1.jpg"
+}
+function aabv11() {
+    document.getElementById("aabv8").src = "Image/Adidas/Men/AdidasLiteRacer2.jpg"
+}
+// ADIDAS LITE RACER 3.0 SHOES Js End
+
+
+// BATA COMFIT - MEN Js Start
+function Batamen1() {
+    document.getElementById("batamen1").src = "Image/Bata/Men/BataComfit2.webp"
+}
+function Batamen2() {
+    document.getElementById("batamen1").src = "Image/Bata/Men/BataComfit.webp"
+}
+function bcm1() {
+    document.getElementById("bcm0").src = "Image/Bata/Men/BataComfit.webp"
+}
+function bcm2() {
+    document.getElementById("bcm0").src = "Image/Bata/Men/BataComfit1.webp"
+}
+function bcm3() {
+    document.getElementById("bcm0").src = "Image/Bata/Men/BataComfit2.webp"
+}
+// BATA COMFIT - MEN Js End
+
+
+// Bata Northstar - Men Js Start
+function Batamen3() {
+    document.getElementById("batamen2").src = "Image/Bata/Men/BataNorthStart2.webp"
+}
+function Batamen4() {
+    document.getElementById("batamen2").src = "Image/Bata/Men/BataNorthStart.webp"
+}
+function bcm5() {
+    document.getElementById("bcm4").src = "Image/Bata/Men/BataNorthStart.webp"
+}
+function bcm6() {
+    document.getElementById("bcm4").src = "Image/Bata/Men/BataNorthStart1.webp"
+}
+function bcm7() {
+    document.getElementById("bcm4").src = "Image/Bata/Men/BataNorthStart2.webp"
+}
+// Bata Northstar - Men Js End
+
+
+// Bata Power - Men Js Start
+function Batamen5() {
+    document.getElementById("batamen3").src = "Image/Bata/Men/BataPower2.webp"
+}
+function Batamen6() {
+    document.getElementById("batamen3").src = "Image/Bata/Men/BataPower.webp"
+}
+function bcm9() {
+    document.getElementById("bcm8").src = "Image/Bata/Men/BataPower.webp"
+}
+function bcm10() {
+    document.getElementById("bcm8").src = "Image/Bata/Men/BataPower1.webp"
+}
+function bcm11() {
+    document.getElementById("bcm8").src = "Image/Bata/Men/BataPower2.webp"
+}
+// Bata Power - Men Js End
+
+
+// Bata Red Label - Men Js Start
+function Batamen7() {
+    document.getElementById("batamen4").src = "Image/Bata/Men/BataRedLabel2.webp"
+}
+function Batamen8() {
+    document.getElementById("batamen4").src = "Image/Bata/Men/BataRedLabel.webp"
+}
+function bcm13() {
+    document.getElementById("bcm12").src = "Image/Bata/Men/BataRedLabel.webp"
+}
+function bcm14() {
+    document.getElementById("bcm12").src = "Image/Bata/Men/BataRedLabel1.webp"
+}
+function bcm15() {
+    document.getElementById("bcm12").src = "Image/Bata/Men/BataRedLabel2.webp"
+}
+// Bata Red Label - Men Js End
+
+
+// Bata Weinbrenner - Men Js Start
+function Batamen9() {
+    document.getElementById("batamen5").src = "Image/Bata/Men/BataWeinbrenner2.webp"
+}
+function Batamen10() {
+    document.getElementById("batamen5").src = "Image/Bata/Men/BataWeinbrenner.webp"
+}
+function bcm17() {
+    document.getElementById("bcm16").src = "Image/Bata/Men/BataWeinbrenner.webp"
+}
+function bcm18() {
+    document.getElementById("bcm16").src = "Image/Bata/Men/BataWeinbrenner1.webp"
+}
+function bcm19() {
+    document.getElementById("bcm16").src = "Image/Bata/Men/BataWeinbrenner2.webp"
+}
+// Bata Weinbrenner - Men Js End
+
+
+// Crocs Classic Boot Js Start
+function Crocsmen1() {
+    document.getElementById("crocsmen1").src = "Image/crocs/Men/ClassicBoot2.jpg"
+}
+function Crocsmen2() {
+    document.getElementById("crocsmen1").src = "Image/crocs/Men/ClassicBoot.jpg"
+}
+function ccb1() {
+    document.getElementById("ccb0").src = "Image/crocs/Men/ClassicBoot.jpg"
+}
+function ccb2() {
+    document.getElementById("ccb0").src = "Image/crocs/Men/ClassicBoot1.jpg"
+}
+function ccb3() {
+    document.getElementById("ccb0").src = "Image/crocs/Men/ClassicBoot2.jpg"
+}
+// Crocs Classic Boot Js End
+
+
+// Crocs Echo Boot Js Start
+function Crocsmen3() {
+    document.getElementById("crocsmen2").src = "Image/crocs/Men/EchoBoost2.jpg"
+}
+function Crocsmen4() {
+    document.getElementById("crocsmen2").src = "Image/crocs/Men/EchoBoost.jpg"
+}
+function ccb5() {
+    document.getElementById("ccb4").src = "Image/crocs/Men/EchoBoost.jpg"
+}
+function ccb6() {
+    document.getElementById("ccb4").src = "Image/crocs/Men/EchoBoost1.jpg"
+}
+function ccb7() {
+    document.getElementById("ccb4").src = "Image/crocs/Men/EchoBoost2.jpg"
+}
+// Crocs Echo Boot Js End
+
+
+// Fila MB Free Guard Sculpted Js Start
+function Filamen1() {
+    document.getElementById("filamen1").src = "Image/Fila/Men/MbFreeGuardSculpted2.jpg"
+}
+function Filamen2() {
+    document.getElementById("filamen1").src = "Image/Fila/Men/MbFreeGuardSculpted.jpg"
+}
+function fms1() {
+    document.getElementById("fms0").src = "Image/Fila/Men/MbFreeGuardSculpted.jpg"
+}
+function fms2() {
+    document.getElementById("fms0").src = "Image/Fila/Men/MbFreeGuardSculpted1.jpg"
+}
+function fms3() {
+    document.getElementById("fms0").src = "Image/Fila/Men/MbFreeGuardSculpted2.jpg"
+}
+// Fila MB Free Guard Sculpted Js End
+
+
+// Fila Stackhouse Speghetti Js Start
+function Filamen3() {
+    document.getElementById("filamen2").src = "Image/Fila/Men/StackhouseSpaghetti2.jpg"
+}
+function Filamen4() {
+    document.getElementById("filamen2").src = "Image/Fila/Men/StackhouseSpaghetti.jpg"
+}
+function fms5() {
+    document.getElementById("fms4").src = "Image/Fila/Men/StackhouseSpaghetti.jpg"
+}
+function fms6() {
+    document.getElementById("fms4").src = "Image/Fila/Men/StackhouseSpaghetti1.jpg"
+}
+function fms7() {
+    document.getElementById("fms4").src = "Image/Fila/Men/StackhouseSpaghetti2.jpg"
+}
+// Fila Stackhouse Speghetti Js End
+
+
+// Fila Targa NT Js Start
+function Filamen5() {
+    document.getElementById("filamen3").src = "Image/Fila/Men/TargaNt2.jpg"
+}
+function Filamen6() {
+    document.getElementById("filamen3").src = "Image/Fila/Men/TargaNt.jpg"
+}
+function fms9() {
+    document.getElementById("fms8").src = "Image/Fila/Men/TargaNt.jpg"
+}
+function fms10() {
+    document.getElementById("fms8").src = "Image/Fila/Men/TargaNt1.jpg"
+}
+function fms11() {
+    document.getElementById("fms8").src = "Image/Fila/Men/TargaNt2.jpg"
+}
+// Fila Targa NT Js End
+
+
+// Fila Targa NT Js Start
+function Filamen7() {
+    document.getElementById("filamen4").src = "Image/Fila/Men/Tennis88VtgLow2.jpg"
+}
+function Filamen8() {
+    document.getElementById("filamen4").src = "Image/Fila/Men/Tennis88VtgLow.jpg"
+}
+function fms13() {
+    document.getElementById("fms12").src = "Image/Fila/Men/Tennis88VtgLow.jpg"
+}
+function fms14() {
+    document.getElementById("fms12").src = "Image/Fila/Men/Tennis88VtgLow1.jpg"
+}
+function fms15() {
+    document.getElementById("fms12").src = "Image/Fila/Men/Tennis88VtgLow2.jpg"
+}
+// Fila Targa NT Js End
+
+
+// Nike Air Jorden 1 Low SE Js Start
+function Nikemen1() {
+    document.getElementById("nikemen1").src = "Image/Nike/Men/NikeAirJordan1LowSe2.png"
+}
+function Nikemen2() {
+    document.getElementById("nikemen1").src = "Image/Nike/Men/NikeAirJordan1LowSe.png"
+}
+function nms1() {
+    document.getElementById("nms0").src = "Image/Nike/Men/NikeAirJordan1LowSe.png"
+}
+function nms2() {
+    document.getElementById("nms0").src = "Image/Nike/Men/NikeAirJordan1LowSe1.jpeg"
+}
+function nms3() {
+    document.getElementById("nms0").src = "Image/Nike/Men/NikeAirJordan1LowSe2.png"
+}
+// Nike Air Jorden 1 Low SE Js End
+
+
+// Nike Air Jorden 1 Mid Js Start
+function Nikemen3() {
+    document.getElementById("nikemen2").src = "Image/Nike/Men/NikeAirJordan1Mid2.jpeg"
+}
+function Nikemen4() {
+    document.getElementById("nikemen2").src = "Image/Nike/Men/NikeAirJordan1Mid.jpeg"
+}
+function nms5() {
+    document.getElementById("nms4").src = "Image/Nike/Men/NikeAirJordan1Mid.jpeg"
+}
+function nms6() {
+    document.getElementById("nms4").src = "Image/Nike/Men/NikeAirJordan1Mid1.jpeg"
+}
+function nms7() {
+    document.getElementById("nms4").src = "Image/Nike/Men/NikeAirJordan1Mid2.jpeg"
+}
+// Nike Air Jorden 1 Mid Js End
+
+
+// Nike Air Jorden 1 Mid Js Start
+function Nikemen5() {
+    document.getElementById("nikemen3").src = "Image/Nike/Men/NikeJordan23-72.png"
+}
+function Nikemen6() {
+    document.getElementById("nikemen3").src = "Image/Nike/Men/NikeJordan23-7.png"
+}
+function nms9() {
+    document.getElementById("nms8").src = "Image/Nike/Men/NikeJordan23-7.png"
+}
+function nms10() {
+    document.getElementById("nms8").src = "Image/Nike/Men/NikeJordan23-71.png"
+}
+function nms11() {
+    document.getElementById("nms8").src = "Image/Nike/Men/NikeJordan23-72.png"
+}
+// Nike Air Jorden 1 Mid Js End
+
+
+// Nike Air Jorden 1 Mid Js Start
+function Pumamen1() {
+    document.getElementById("pumamen1").src = "Image/Puma/Men/PumaPalermoLeather1.jpg"
+}
+function Pumamen2() {
+    document.getElementById("pumamen1").src = "Image/Puma/Men/PumaPalermoLeather.jpg"
+}
+function pms2() {
+    document.getElementById("pms1").src = "Image/Puma/Men/PumaPalermoLeather.jpg"
+}
+function pms3() {
+    document.getElementById("pms1").src = "Image/Puma/Men/PumaPalermoLeather1.jpg"
+}
+// Nike Air Jorden 1 Mid Js End
+
+
+// Nike Air Jorden 1 Mid Js Start
+function Pumamen3() {
+    document.getElementById("pumamen2").src = "Image/Puma/Men/PumaRomaBasic2.jpg"
+}
+function Pumamen4() {
+    document.getElementById("pumamen2").src = "Image/Puma/Men/PumaRomaBasic.jpg"
+}
+function pms5() {
+    document.getElementById("pms4").src = "Image/Puma/Men/PumaRomaBasic.jpg"
+}
+function pms6() {
+    document.getElementById("pms4").src = "Image/Puma/Men/PumaRomaBasic1.jpg"
+}
+function pms7() {
+    document.getElementById("pms4").src = "Image/Puma/Men/PumaRomaBasic2.jpg"
+}
+// Nike Air Jorden 1 Mid Js End
+
+
+// Nike Air Jorden 1 Mid Js Start
+function Pumamen5() {
+    document.getElementById("pumamen3").src = "Image/Puma/Men/PumaSuedeClassicXXI2.jpg"
+}
+function Pumamen6() {
+    document.getElementById("pumamen3").src = "Image/Puma/Men/PumaSuedeClassicXXI.jpg"
+}
+function pms9() {
+    document.getElementById("pms8").src = "Image/Puma/Men/PumaSuedeClassicXXI.jpg"
+}
+function pms10() {
+    document.getElementById("pms8").src = "Image/Puma/Men/PumaSuedeClassicXXI1.jpg"
+}
+function pms11() {
+    document.getElementById("pms8").src = "Image/Puma/Men/PumaSuedeClassicXXI2.jpg"
+}
+// Nike Air Jorden 1 Mid Js End
+
+
+// Nike Air Jorden 1 Mid Js Start
+function Pumamen7() {
+    document.getElementById("pumamen4").src = "Image/Puma/Men/PumaSuedeMidXXI2.jpg"
+}
+function Pumamen8() {
+    document.getElementById("pumamen4").src = "Image/Puma/Men/PumaSuedeMidXXI.jpg"
+}
+function pms13() {
+    document.getElementById("pms12").src = "Image/Puma/Men/PumaSuedeMidXXI.jpg"
+}
+function pms14() {
+    document.getElementById("pms12").src = "Image/Puma/Men/PumaSuedeMidXXI1.jpg"
+}
+function pms15() {
+    document.getElementById("pms12").src = "Image/Puma/Men/PumaSuedeMidXXI2.jpg"
+}
+// Nike Air Jorden 1 Mid Js End
+
+
+// Nike Air Jorden 1 Mid Js Start
+function Pumamen9() {
+    document.getElementById("pumamen5").src = "Image/Puma/Men/PumaSuedeVTGHarrisTweed1.jpg"
+}
+function Pumamen10() {
+    document.getElementById("pumamen5").src = "Image/Puma/Men/PumaSuedeVTGHarrisTweed.jpg"
+}
+function pms17() {
+    document.getElementById("pms16").src = "Image/Puma/Men/PumaSuedeVTGHarrisTweed.jpg"
+}
+function pms18() {
+    document.getElementById("pms16").src = "Image/Puma/Men/PumaSuedeVTGHarrisTweed1.jpg"
+}
+// Nike Air Jorden 1 Mid Js End
+
+
+// Nike Air Jorden 1 Mid Js Start
+function Pumamen11() {
+    document.getElementById("pumamen6").src = "Image/Puma/Men/PumaSuedeXL1.jpg"
+}
+function Pumamen12() {
+    document.getElementById("pumamen6").src = "Image/Puma/Men/PumaSuedeXL.jpg"
+}
+function pms23() {
+    document.getElementById("pms22").src = "Image/Puma/Men/PumaSuedeXL.jpg"
+}
+function pms24() {
+    document.getElementById("pms22").src = "Image/Puma/Men/PumaSuedeXL1.jpg"
+}
+
+// Nike Air Jorden 1 Mid Js End
+
+
+// Nike Air Jorden 1 Mid Js Start
+function Sorelmen1() {
+    document.getElementById("sorelmen1").src = "Image/Sorel/Men/SorelExplorerNext2.jpeg"
+}
+function Sorelmen2() {
+    document.getElementById("sorelmen1").src = "Image/Sorel/Men/SorelExplorerNext.jpeg"
+}
+function sms2() {
+    document.getElementById("sms1").src = "Image/Sorel/Men/SorelExplorerNext.jpeg"
+}
+function sms3() {
+    document.getElementById("sms1").src = "Image/Sorel/Men/SorelExplorerNext1.webp"
+}
+function sms4() {
+    document.getElementById("sms1").src = "Image/Sorel/Men/SorelExplorerNext2.jpeg"
+}
+// Nike Air Jorden 1 Mid Js End
+
+
+// Nike Air Jorden 1 Mid Js Start
+function Sorelmen3() {
+    document.getElementById("sorelmen2").src = "Image/Sorel/Men/SorelMacHillLiteHiker2.jpeg"
+}
+function Sorelmen4() {
+    document.getElementById("sorelmen2").src = "Image/Sorel/Men/SorelMacHillLiteHiker.webp"
+}
+function sms6() {
+    document.getElementById("sms5").src = "Image/Sorel/Men/SorelMacHillLiteHiker.webp"
+}
+function sms7() {
+    document.getElementById("sms5").src = "Image/Sorel/Men/SorelMacHillLiteHiker1.jpeg"
+}
+function sms8() {
+    document.getElementById("sms5").src = "Image/Sorel/Men/SorelMacHillLiteHiker2.jpeg"
+}
+// Nike Air Jorden 1 Mid Js End
+
+
+// Nike Air Jorden 1 Mid Js Start
+function Sorelmen5() {
+    document.getElementById("sorelmen3").src = "Image/Sorel/Men/SorelMetroIIChukka2.jpeg"
+}
+function Sorelmen6() {
+    document.getElementById("sorelmen3").src = "Image/Sorel/Men/SorelMetroIIChukka.webp"
+}
+function sms10() {
+    document.getElementById("sms9").src = "Image/Sorel/Men/SorelMetroIIChukka.webp"
+}
+function sms11() {
+    document.getElementById("sms9").src = "Image/Sorel/Men/SorelMetroIIChukka1.webp"
+}
+function sms12() {
+    document.getElementById("sms9").src = "Image/Sorel/Men/SorelMetroIIChukka2.jpeg"
+}
+// Nike Air Jorden 1 Mid Js End
+
+// Nike Air Jorden 1 Mid Js Start
+function Sorelmen5() {
+    document.getElementById("sorelmen3").src = "Image/Sorel/Men/SorelMetroIILow2.jpeg"
+}
+function Sorelmen6() {
+    document.getElementById("sorelmen3").src = "Image/Sorel/Men/SorelMetroIILow.webp"
+}
+function sms10() {
+    document.getElementById("sms9").src = "Image/Sorel/Men/SorelMetroIILow.webp"
+}
+function sms11() {
+    document.getElementById("sms9").src = "Image/Sorel/Men/SorelMetroIILow1.webp"
+}
+function sms12() {
+    document.getElementById("sms9").src = "Image/Sorel/Men/SorelMetroIILow2.jpeg"
+}
+// Nike Air Jorden 1 Mid Js End
+
+// ContectUs Js Start
+function formvalidate() {
+    var name = document.getElementById("uname").value
+    var email = document.getElementById("uemail").value
+    var subject = document.getElementById("usubject").value
+    var message = document.getElementById("umessage").value
+
+    if (name == "") {
+        document.getElementById("errorname").innerHTML = "Name field is Required*"
+    }
+
+    else if (email == "") {
+        document.getElementById("erroremail").innerHTML = "Email field is Required*"
+        document.getElementById("errorname").innerHTML = ""
+
+    }
+
+    else if (subject == "") {
+        document.getElementById("errorsubject").innerHTML = "Subject field is Required*"
+        document.getElementById("erroremail").innerHTML = ""
+    }
+
+    else if (message == "") {
+        document.getElementById("errormessage").innerHTML = "Message field is Required*"
+        document.getElementById("errorsubject").innerHTML = ""
+    }
+
+    else if (name != "" && email != "" && subject != "" && message != "") {
+        document.getElementById("msg").style.display = "block"
+        var name = document.getElementById("uname").value = ''
+        var email = document.getElementById("uemail").value = ''
+        var subject = document.getElementById("usubject").value = ''
+        var message = document.getElementById("umessage").value = ''
+    }
+    else {
+        document.getElementById("errormessage").innerHTML = ""
+    }
+}
+
+function nameevent() {
+    var name = document.getElementById("uname").value
+
+
+    if (name != "") {
+        document.getElementById("errorname").innerHTML = ""
+
+    }
+}
+
+function emailevent() {
+    var email = document.getElementById("uemail").value
+
+
+    if (email != "") {
+        document.getElementById("erroremail").innerHTML = ""
+
+    }
+}
+
+function subjectevent() {
+    var subject = document.getElementById("usubject").value
+
+
+    if (subject != "") {
+        document.getElementById("errorsubject").innerHTML = ""
+
+    }
+}
+
+function messageevent() {
+    var message = document.getElementById("umessage").value
+
+
+    if (message != "") {
+        document.getElementById("errormessage").innerHTML = ""
+
+    }
+}
+// ContectUs Js End
